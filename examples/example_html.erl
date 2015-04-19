f32b4eaa0d6171d60934d91e13eea205d808728a
Stanislaw Klekot <dozzie@jarowit.net>
2015-04-19 19:19:45 +0200

    Added HTML and nginx.conf examples.

--- examples/example_html.erl
+++ examples/example_html.erl
@@ -2,6 +2,39 @@
 %%% @doc
 %%%   Example HTML to check Server-Sent Events in the browser.
 %%%
+%```
+%<html>
+%<head>
+%<title>server-sent events test</title>
+%<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+%<style>
+%
+%pre {
+%  border: solid 1px black;
+%}
+%
+%</style>
+%<script>
+%
+%  var es = new EventSource("/events");
+%
+%  es.onmessage = function(e) {
+%    var logs = document.getElementById("logs");
+%    logs.textContent += e.data + "\n";
+%  };
+%
+%</script>
+%</head>
+%<body>
+%
+%<h1>Test of server-sent events</h1>
+%
+%<pre id="logs">
+%</pre>
+%
+%</body>
+%</html>
+%'''
 %%% @end
 %%%---------------------------------------------------------------------------
 
--- examples/example_nginx.erl
+++ examples/example_nginx.erl
@@ -2,6 +2,25 @@
 %%% @doc
 %%%   Example config for nginx.
 %%%
+%```
+%server {
+%  # ...
+%
+%  location / {
+%    proxy_pass http://127.0.0.1:1080/;
+%  }
+%
+%  location /events {
+%    proxy_pass http://127.0.0.1:1080/events;
+%    proxy_buffering off;
+%    proxy_cache off;
+%    proxy_http_version 1.1;
+%    chunked_transfer_encoding off;
+%  }
+%
+%  # ...
+%}
+%'''
 %%% @end
 %%%---------------------------------------------------------------------------
 
